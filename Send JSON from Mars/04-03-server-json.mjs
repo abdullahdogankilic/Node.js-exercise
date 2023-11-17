51aec7f1bc8515527f776f8b558f2829d5bbc309
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-11-17 23:29:49 +0300

    1

---
+++ Send JSON from Mars/04-03-server-json.mjs
@@ -0,0 +1,19 @@
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  response.setHeader("Content-Type", "application/json");
+
+  const jsonResponseBody = JSON.stringify({ location: "Mars" });
+  response.setHeader("Content-Length", Buffer.byteLength(jsonResponseBody));
+  // Content-Length: 19
+
+  response.end(jsonResponseBody);
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
