3ba163b72034ec59dcb0ef2666ace19392c21703
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-12-01 20:23:58 +0300

    1

---
+++ JWT Auth part 1/vite-project/src/server.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import multer from "multer";
+import path from "path";
+import {
+  setupDb,
+  getAll,
+  getOneById,
+  create,
+  updateById,
+  deleteById,
+  uploadImage,
+} from "./controllers/planets";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: function (_req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+setupDb();
+
+app.get("/planets", getAll);
+app.get("/planets/:id", getOneById);
+app.post("/planets", create);
+app.put("/planets/:id", updateById);
+app.delete("/planets/:id", deleteById);
+app.post("/planets/:id/image", upload.single("image"), uploadImage);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
